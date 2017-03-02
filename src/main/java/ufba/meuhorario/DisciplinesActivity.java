dfbf243875854adb62b450a230e0dfd3b0879e58
dnovaes <novaesdigo@gmail.com>
2017-03-02 00:57:19 -0300

    > Added a different approach on getting json files by getting data in > local folder. (applied to disciplines)   > added functional behavior for disciplineActivity. Now its showing   > full disciplines contained in the jsonFile disciplines.

--- src/main/java/ufba/meuhorario/DisciplinesActivity.java
+++ src/main/java/ufba/meuhorario/DisciplinesActivity.java
@@ -3,11 +3,20 @@ package ufba.meuhorario;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import ufba.meuhorario.adapter.ListDisciplinesAdapter;
@@ -19,8 +28,9 @@ import ufba.meuhorario.model.Discipline;
  * Created by Diego Novaes on 28/02/2017.
  */
 public class DisciplinesActivity extends AppCompatActivity {
-    private String DisciplinesJson = "https://gist.githubusercontent.com/dnovaes/419fc613aee50ae971cbfed466c8e512/raw/b0aa21ca34ddcf3f540fa25c73872eb6956f5ab1/meuhorario_disciplines";
-    private String DisciplinesCoursesJson = "https://gist.githubusercontent.com/dnovaes/fdea84cf2485a9350addd0d42f87c5c0/raw/d446f1ffa3cf1cb21f671afbc26370d956e726b0/meuhorario_courses_disciplines";
+    private String DisciplinesJson = "https://gist.githubusercontent.com/dnovaes/419fc613aee50ae971cbfed466c8e512/raw/8352748e5d26e5f38182cadf115b66a6275ef686/meuhorario_disciplines.json";
+    public String jsonDisciplinesArrayName = "disciplines";
+    private String DisciplinesCoursesJson = "https://gist.githubusercontent.com/dnovaes/fdea84cf2485a9350addd0d42f87c5c0/raw/282dfae3645d690af969d8b7bc49f9b83479b337/meuhorario_course_disciplines.json";
     private ListView disciplinesList;
     private Long course_id;
 
@@ -35,6 +45,9 @@ public class DisciplinesActivity extends AppCompatActivity {
         course_id = course.getId();
 
         disciplinesList = (ListView)findViewById(R.id.list_courses);
+
+        TextView titleView = (TextView) findViewById(R.id.activity_title);
+        titleView.setText(course.getName());
     }
 
     @Override
@@ -52,10 +65,31 @@ public class DisciplinesActivity extends AppCompatActivity {
         if(disciplines.isEmpty()){
             // Creating a anonymous JsonParser instance
             // download json and insert into SQLite
-            Toast.makeText(this, "Atualizando dados de 'disciplines' ...", Toast.LENGTH_SHORT).show();
-            new JSONParser(this, DisciplinesJson, "disciplines").execute();
-            new JSONParser(this, DisciplinesCoursesJson, "course_disciplines").execute();
-            //Add a window here that shows the return of JSONParser and force the user to click to back to the DisciplinesActivity and calls the onResume method
+            //new JSONParser(this, DisciplinesJson, "disciplines").execute();
+            //new JSONParser(this, DisciplinesCoursesJson, "course_disciplines").execute();
+            InputStream is = this.getResources().openRawResource(R.raw.meuhorario_disciplines);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            int ctr;
+            try {
+                ctr = is.read();
+                while (ctr != -1) {
+                    byteArrayOutputStream.write(ctr);
+                    ctr = is.read();
+                }
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                JSONObject jsonObj = new JSONObject(byteArrayOutputStream.toString());
+                JSONArray jsonArray = jsonObj.getJSONArray(jsonDisciplinesArrayName);
+                Log.e("countDisciplines", String.valueOf(dao.getCountDisciplines()));
+                if (!(dao.getCountDisciplines() > 0)){
+                    JSONParser.StoreinDatabase(jsonArray, dao, jsonDisciplinesArrayName);
+                }
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         dao.close();
 
